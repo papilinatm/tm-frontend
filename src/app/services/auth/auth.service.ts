4bdf08aff7d1f09cfb8ee1941f8ba6b3f95d5d06
papilinatm <papilinatm@users.noreply.github.com>
2023-02-16 15:28:27 +0300

    fix ts-md5

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse} from '@angular/common/http';
 import { SRV_URL } from 'src/app/config';
-import {Md5} from 'ts-md5/dist/md5';
+import {Md5} from 'ts-md5';
 import { Observable, of } from 'rxjs';
-import { map,catchError } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,26 +12,26 @@ export class AuthService {
 
   constructor(private _http: HttpClient) { }
 
-  public login(info: { login: string, password: string }):Observable<number> { 
-    
-    info.password = Md5.hashStr(info.password) as string
-    
+  public login(info: { login: string, password: string }): Observable<number> {
+
+    info.password = Md5.hashStr(info.password) as string;
+
     return this._http.post<any>("/api/auth/", info, {observe: 'response'})
             .pipe(
-              map(res=> 
+              map(res =>
               {
                 if (res.status == 200)
-                  localStorage.setItem("token", res.body.token);                
+                  localStorage.setItem("token", res.body.token);
                 return res.status;
               }),
               catchError(error => {
                 return of((error as HttpResponse<any>).status);
               }
               )
-            )
+            );
   }
-  
+
   public sendTestRequest(){
-    this._http.get("/weatherforecast/summaries").subscribe(res=> alert(res));
+    this._http.get("/weatherforecast/summaries").subscribe(res => alert(res));
   }
 }
